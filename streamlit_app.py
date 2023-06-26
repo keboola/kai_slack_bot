d07686c4e193851a42ca55eee47535ee1cbc0e34
marc <marc@keboola.com>
2023-06-26 15:38:52 +0200

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import sys
 import subprocess
 
-subprocess.run(["python3", "app.py"],  stderr=sys.stdout.buffer, stdout=sys.stdout.buffer)
+subprocess.run(["python3", "/home/appuser/app/app.py"],  stderr=sys.stdout.buffer, stdout=sys.stdout.buffer)
